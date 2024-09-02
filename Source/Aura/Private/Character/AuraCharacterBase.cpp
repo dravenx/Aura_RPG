cd3e6000c65a602ec03d9e69c3ab376e710d35df
les.ellis@haenir.com <les.ellis@haenir.com>
2024-09-02 15:07:14 +0100

    changed folder structure

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,20 @@
+// Copyright 2024 XTendedRealities Ltd 
+
+
+#include "AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
